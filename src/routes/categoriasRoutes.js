c48f6574181770798758ff05e7a6801f31f8b082
Favionetah <faviosandy30@gmail.com>
2025-08-24 00:27:28 -0400

    Se agregó PUT y POST para Categorias

--- src/routes/categoriasRoutes.js
+++ src/routes/categoriasRoutes.js
@@ -10,4 +10,33 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/", (req, res) => {
+  const { nombre } = req.body;
+
+  const sql = `INSERT INTO categorias (nombre) VALUES (?)`
+  db.query(sql, [nombre], (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(500).json({error: "Error en el Servidor"})
+    }
+    res.status(201).json({message: "Categoria Creada Exitosamente!! :)", id: result.insertId})
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { nombre } = req.body;
+
+  const sql = `UPDATE categorias 
+  SET nombre = ? WHERE id_categoria = ?`;
+
+  db.query(sql, [nombre, id], (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(500).json({ error: "Error al Actualizar la Categoria" });
+    }
+    res.status(201).json({ message: "La Categoria se actualizo Exitosamente!! :)" })
+  });
+});
+
 module.exports = router;
