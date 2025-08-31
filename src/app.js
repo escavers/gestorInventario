2f4a7e3ab82081c6584405d7569fe4e7a2ce089f
Favionetah <faviosandy30@gmail.com>
2025-08-31 14:25:31 -0400

    Se agrego el cors y sus dependencias json

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
-const express = require("express");
-const app = express();
-const puerto = 3000;
+    const express = require("express");
+    const app = express();
+    const cors = require("cors"); //agregue la importacion de "cors"
+    const puerto = 3000;
+    
+    app.use(cors()); //aqui usamos el cors (para que otros archivos (dominios) puedan ingresas a estos recursos)
+    app.use(express.json());
 
-app.use(express.json());
+    const usuariosRoutes = require("./routes/usuariosRoutes");
+    const proveedoresRoutes = require("./routes/proveedoresRoutes");
+    const categoriasRoutes = require("./routes/categoriasRoutes");
+    const productosRoutes = require("./routes/productosRoutes");
+    const comprasRoutes = require("./routes/comprasRoutes");
+    const ventasRoutes = require("./routes/ventasRoutes");
 
-const usuariosRoutes = require("./routes/usuariosRoutes");
-const proveedoresRoutes = require("./routes/proveedoresRoutes");
-const categoriasRoutes = require("./routes/categoriasRoutes");
-const productosRoutes = require("./routes/productosRoutes");
-const comprasRoutes = require("./routes/comprasRoutes");
-const ventasRoutes = require("./routes/ventasRoutes");
+    app.use("/usuarios", usuariosRoutes);
+    app.use("/proveedores", proveedoresRoutes);
+    app.use("/categorias", categoriasRoutes);
+    app.use("/productos", productosRoutes);
+    app.use("/compras", comprasRoutes);
+    app.use("/ventas", ventasRoutes);
 
-app.use("/usuarios", usuariosRoutes);
-app.use("/proveedores", proveedoresRoutes);
-app.use("/categorias", categoriasRoutes);
-app.use("/productos", productosRoutes);
-app.use("/compras", comprasRoutes);
-app.use("/ventas", ventasRoutes);
+    app.listen(puerto, '0.0.0.0', () => {
+        console.log(`Servidor corriendo en http://localhost:${puerto}`);
 
-app.listen(puerto, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en http://localhost:${puerto}`);
-
-});
+    });
