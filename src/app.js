89395e38e9a1efe0b44218e3fe7ffb86ae34b6cc
Favionetah <faviosandy30@gmail.com>
2025-09-02 09:52:33 -0400

    Frontend y app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
     const express = require("express");
     const app = express();
+    const path = require('path');
     const cors = require("cors"); //agregue la importacion de "cors"
     const puerto = 3000;
     
     app.use(cors()); //aqui usamos el cors (para que otros archivos (dominios) puedan ingresas a estos recursos)
     app.use(express.json());
+
+    app.use(express.static(path.join(__dirname, 'Front_Proyecto_TW')));
 
     const usuariosRoutes = require("./routes/usuariosRoutes");
     const proveedoresRoutes = require("./routes/proveedoresRoutes");
@@ -19,6 +22,10 @@
     app.use("/productos", productosRoutes);
     app.use("/compras", comprasRoutes);
     app.use("/ventas", ventasRoutes);
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'Front_Proyecto_TW', 'index.html'));
+    });
 
     app.listen(puerto, '0.0.0.0', () => {
         console.log(`Servidor corriendo en http://localhost:${puerto}`);
